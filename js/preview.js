4c51293c391533115535855342373d99a5ebaa7c
GuruSam <artem7127.com@inbox.ru>
2019-10-15 18:56:32 +0500

    Превью изображений при загрузке

---
+++ js/preview.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var avatarChooser = document.querySelector('input[id="avatar"]');
+  var avatarPreview = document.querySelector('.ad-form-header__preview > img');
+
+  avatarChooser.addEventListener('change', function () {
+    var file = avatarChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        avatarPreview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
